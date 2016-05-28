b221acb1848e7d436dfab4e786d16fb0a5a79f03
josseline <josselinelucia@gmail.com>
2016-05-28 16:30:18 -0600

    bodegas servicio y component

---
+++ src/app/scripts/app/mantenimientos/bodegas-listado.component.ts
@@ -0,0 +1,46 @@
+﻿import { Component, OnInit } from "@angular/core"
+import { ROUTER_DIRECTIVES } from "@angular/router-deprecated"
+import { BodegasServicio } from "./bodegas.servicio"
+import { OrdenarServicio} from "../ordenar.servicio"
+import { BodegaResumen } from "./modelos"
+import { PaginacionResultado, Dictionary } from "../modelos"
+import { PaginaComponent } from "../pagina.component"
+import { PaginacionComponent } from "../paginacion.component"
+
+@Component({
+    selector: 'bodegas-listado',
+    templateUrl: 'app/mantenimientos/bodegas-listado.template.html',
+    providers: [BodegasServicio],
+    directives: [ROUTER_DIRECTIVES, PaginaComponent, PaginacionComponent]
+})
+export class BodegasListadoComponent implements OnInit {
+    private pagina: number = null;
+    bodegas: PaginacionResultado<BodegaResumen>;
+    ordenar: OrdenarServicio;
+
+    constructor(private bodegasServicio: BodegasServicio) {
+        this.bodegas = {};
+        this.ordenar = new OrdenarServicio();
+        this.ordenar.alCambiarOrden.subscribe(this.cambiarOrden);
+    }
+
+    ngOnInit() {
+        this.obtenerBodegas();
+    }
+
+    obtenerBodegas(pagina?: number, campo?: string) {
+        var ordenamiento = this.ordenar.campos;
+        this.pagina = pagina;
+        this.bodegasServicio.obtenerTodas({ pagina: pagina, ordenamiento: ordenamiento }).subscribe(x => {
+            this.bodegas = x;
+        })
+    }
+    cambiarPagina(pagina: number) {
+        this.obtenerBodegas(pagina);
+    }
+
+    cambiarOrden = (columna: string) => {
+        this.obtenerBodegas(this.pagina);
+    }
+
+}
